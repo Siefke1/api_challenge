9222fa43947b1e3c7078d38cd87bbc76081f7ae8
Leon Langhoff <leon.langhoff@posteo.de>
2022-06-02 22:32:13 +0200

    appointment_confirmation model v1

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_02_202145) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_02_203135) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "appointment_confirmations", force: :cascade do |t|
+    t.string "name"
+    t.string "phone"
+    t.string "address"
+    t.datetime "time"
+    t.float "latitude"
+    t.float "longitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "realtors", force: :cascade do |t|
     t.string "name"
